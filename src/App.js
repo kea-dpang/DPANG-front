198bbc40c13b5a2ea0950c150581e102d9287ddc
yougi8 <yougi8@naver.com>
2024-01-14 22:01:17 +0900

    [feat] 공통컴포넌트 - 관리자 sidebar (이벤트관리 페이지 라우팅)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EventPage from "./pages/admin/eventPage/Index"
+import EventListPage from "./pages/admin/eventPage/list/Index"
+import EventEnrollPage from "./pages/admin/eventPage/enroll/Index"
+import EventEditPage from "./pages/admin/eventPage/edit/Index"
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          {/* 관리자 이벤트 페이지 */}
+          <Route path="/admin/event" element={<EventPage />}>
+              <Route path="list" element={<EventListPage/>}/>
+              <Route path="enroll" element={<EventEnrollPage />}/>
+              <Route path="edit" element={<EventEditPage />}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
