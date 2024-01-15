2ce30561d72f2a862fc35ed7859705a19e674bac
yougi8 <yougi8@naver.com>
2024-01-15 13:42:38 +0900

    [chore] app.js 코드 충돌 제거

--- src/App.js
+++ src/App.js
@@ -9,11 +9,6 @@ import DirectAskPage from "./pages/user/myPage/directAsk/Index";
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      {/* <ReviewList /> */}
-      <EnrollReview />
-
-=======
       <BrowserRouter>
         <Routes>
           {/* 관리자 이벤트 페이지 */}
@@ -29,7 +24,6 @@ function App() {
           </Route>
         </Routes>
       </BrowserRouter>
->>>>>>> 816efa8b639c631798bf16de295da3a15499bd2d
     </div>
   );
 }
