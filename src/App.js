4d5b88d5501803e564f665727c2a67dea1e3222a
hogishim <harrythegreat@gachon.ac.kr>
2024-01-18 16:06:06 +0900

    [feat]관리자 페이지 - 취소 상세 페이지 레이아웃 일부 구현

--- src/App.js
+++ src/App.js
@@ -17,6 +17,9 @@ import Leave from "./pages/user/myPage/userInfo/Leave";
 import WishList from "./pages/user/WishList";
 import Detail from './pages/user/myPage/Refund/Detail/Index'
 import ReqRefund from './pages/user/myPage/Refund/Enroll/Index'
+import AdminRefund from './pages/admin/Refund/Index'
+import AdminRefundList from './pages/admin/Refund/List/Index'
+import AdminRefundDetail from './pages/admin/Refund/Detail/Index'
 
 function App() {
   return (
@@ -25,25 +28,31 @@ function App() {
         <Routes>
           {/* 관리자 이벤트 페이지 */}
           <Route path="/admin/event" element={<EventPage />}>
-              <Route path="list" element={<EventListPage/>}/>
-              <Route path="enroll" element={<EventEnrollPage />}/>
-              <Route path="edit" element={<EventEditPage />}/>
+            <Route path="list" element={<EventListPage />} />
+            <Route path="enroll" element={<EventEnrollPage />} />
+            <Route path="edit" element={<EventEditPage />} />
           </Route>
+
+          <Route path="/admin/refund" element={<AdminRefund />}>
+            <Route path="list" element={<AdminRefundList />} />
+            <Route path="detail" element={<AdminRefundDetail />}/>
+          </Route>
+
           {/* <MyPage /> */}
           <Route path="/user/mypage/temp" element={<MyPage />}>
-              <Route path="directask" element={<DirectAskPage />}/>
-              <Route path="userinfo" element={<UserInfoPage />}/>
-              <Route path="userinfo/editpassword" element={<EditPassword />}/>
-              <Route path="userinfo/leave" element={<Leave />}/>
+            <Route path="directask" element={<DirectAskPage />} />
+            <Route path="userinfo" element={<UserInfoPage />} />
+            <Route path="userinfo/editpassword" element={<EditPassword />} />
+            <Route path="userinfo/leave" element={<Leave />} />
 
-              <Route path="review" element={<Review />} />
-              <Route path="review/enroll" element={<EnrollReview />} />
-              <Route path="mileage" element={<Mileage />} />
-              <Route path="mileage/req" element={<MileageReq />} />
-              <Route path="refund"element={<UserRefund />} />
-              <Route path="order" element={<UserOrder />} />
-              <Route path="refund/detail" element={<Detail />} />
-              <Route path="refund/enroll" element={<ReqRefund/>} />
+            <Route path="review" element={<Review />} />
+            <Route path="review/enroll" element={<EnrollReview />} />
+            <Route path="mileage" element={<Mileage />} />
+            <Route path="mileage/req" element={<MileageReq />} />
+            <Route path="refund" element={<UserRefund />} />
+            <Route path="order" element={<UserOrder />} />
+            <Route path="refund/detail" element={<Detail />} />
+            <Route path="refund/enroll" element={<ReqRefund />} />
           </Route>
 
         </Routes>
