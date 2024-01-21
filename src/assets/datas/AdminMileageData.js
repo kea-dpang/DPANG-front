9280a44d3d0ad01b924260e787c27465b58c3e39
hogishim <harrythegreat@gachon.ac.kr>
2024-01-21 15:33:14 +0900

    [feat]관리자 마일리지 신청 관리 페이지 구현

---
+++ src/assets/datas/AdminMileageData.js
@@ -0,0 +1,12 @@
+const MileageList = [
+    { id: 1, kind: '대기', requestdate: '2024-01-08 00:00:00', companyid: '19-76079968', name: '김디팡', payname: '김디팡', money: '100,000' },
+    { id: 2, kind: '대기', requestdate: '2024-01-07 00:00:00', companyid: '19-76079968', name: '김지팡', payname: '김디팡', money: '100,000' },
+    { id: 3, kind: '승인', requestdate: '2024-01-06 00:00:00', companyid: '19-76079968', name: '김팡', payname: '김디팡', money: '100,000' },
+    { id: 4, kind: '승인', requestdate: '2024-01-05 00:00:00', companyid: '19-76079968', name: '디팡', payname: '김디팡', money: '100,000' },
+    { id: 5, kind: '거절', requestdate: '2024-01-04 00:00:00', companyid: '19-76079968', name: '김지', payname: '김디팡', money: '100,000' },
+    { id: 6, kind: '승인', requestdate: '2024-01-03 00:00:00', companyid: '19-76079968', name: '김디', payname: '김디팡', money: '100,000' },
+    { id: 7, kind: '대기', requestdate: '2024-01-02 00:00:00', companyid: '19-76079968', name: '팡', payname: '김디팡', money: '50,000' },
+    { id: 8, kind: '대기', requestdate: '2024-01-01 00:00:00', companyid: '19-76079968', name: '디팡', payname: '김디팡', money: '80,000' },
+  ];
+
+export default MileageList;
