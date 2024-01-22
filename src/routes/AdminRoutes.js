3c56189f12f4154a4b1394315564d511df244fc3
yougi8 <yougi8@naver.com>
2024-01-22 17:26:32 +0900

    [fix] 관리자 라우팅 주소 변경 & 관리자 사이드바 기능 구현

--- src/routes/AdminRoutes.js
+++ src/routes/AdminRoutes.js
@@ -5,36 +5,38 @@ import EventProductEnrollPage from "../pages/admin/eventPage/enroll/Index"
 import EventBrandEnrollPage from "../pages/admin/eventPage/enroll/BrandIndex"
 import EventProductEditPage from "../pages/admin/eventPage/edit/Index"
 import EventBrandEditPage from "../pages/admin/eventPage/edit/BrandIndex"
-import OrderPage from "../pages/admin/orderPage/Index";
-import AdminRefund from '../pages/admin/Refund/Index'
+import OrderPage from "../pages/admin/orderPage/OrderBox";
 import AdminRefundList from '../pages/admin/Refund/List/Index'
 import AdminRefundDetail from '../pages/admin/Refund/Detail/Index'
-import AdminMileage from '../pages/admin/Mileage/Index'
 import AdminMileageView from '../pages/admin/Mileage/List/Index'
-import EventPage from '../pages/admin/eventPage/Index'
+import AdminPage from '../pages/admin/Index'
+import AdminUserListPage from "../pages/admin/userPage/list/Index";
+import AdminUserEditPage from "../pages/admin/userPage/edit/Index";
+import AdminFaqListPage from '../pages/admin/faqPage/list/Index';
 
 const AdminRoutes = () => {
     return (
         <Routes>
-            <Route path="event" element={<EventPage />}>
-                <Route path="list" element={<EventListPage />} />
-                <Route path="enrollproduct" element={<EventProductEnrollPage />} />
-                <Route path="enrollbrand" element={<EventBrandEnrollPage />} />
-                <Route path="editproduct/:eventId" element={<EventProductEditPage />} />
-                <Route path="editbrand/:eventId" element={<EventBrandEditPage />} />
+            <Route path="" element={<AdminPage/>}>
+                {/* 회원관리 */}
+                <Route path="user" element={<AdminUserListPage />}/>
+                <Route path="user/:memberId" element={<AdminUserEditPage />}/>
+                <Route path="mileage" element={<AdminMileageView />}/>
+                {/* 이벤트 관리 */}
+                <Route path="event" element={<EventListPage />}/>
+                <Route path="event/enrollproduct" element={<EventProductEnrollPage />} />
+                <Route path="event/enrollbrand" element={<EventBrandEnrollPage />} />
+                <Route path="event/editproduct/:eventId" element={<EventProductEditPage />} />
+                <Route path="event/editbrand/:eventId" element={<EventBrandEditPage />} />
+                {/* 주문관리 */}
+                <Route path="order" element={<OrderPage />} />
+                {/* 환불관리 */}
+                <Route path="refund" element={<AdminRefundList />}/>
+                <Route path="refund/:id" element={<AdminRefundDetail />} />
+                {/* 고객센터 */}
+                <Route path="faq" element={<AdminFaqListPage/>}/>
+                {/* </Route> */}
             </Route>
-
-            <Route path="refund" element={<AdminRefund />}>
-                <Route path="list" element={<AdminRefundList />} />
-                <Route path="detail/:id" element={<AdminRefundDetail />} />
-            </Route>
-
-            <Route path="mileage" element={<AdminMileage />}>
-                <Route path="list" element={<AdminMileageView />} />
-            </Route>
-
-            <Route path="order" element={<OrderPage />} />
-
         </Routes>
     );
 };
