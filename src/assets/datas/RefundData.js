f7a444b45968c0ae053e2bdcd1eaf221f498c836
hogishim <harrythegreat@gachon.ac.kr>
2024-01-22 21:22:51 +0900

    [feat]사용자 마이페이지 - 환불 리스트 확인 로직 수정(한번에 취소는 한 아이템만)

--- src/assets/datas/RefundData.js
+++ src/assets/datas/RefundData.js
@@ -2,10 +2,12 @@ import img from '../images/tempitemimg.png'
 
 const List = [
 
-    {id: 0, type: "환불", date: "2023.11.13", ordernum: 112232, item: [{img: img, name: "한국시리즈 후드 105", money: 160000, refund: 157000, amt: 2}], category: "단순변심", state: "환불요청"},
-    {id: 1, type: "취소", date: "2023.11.13", ordernum: 112232, item: [{img: img, name: "한국시리즈 후드 105", money: 160000, refund: 157000, amt: 2}, {img: img, name: "한국시리즈 후드 105", money: 160000, refund: 157000, amt: 2}], category: "단순변심", state: "환불요청"}, 
-    {id: 2, type: "환불", date: "2023.11.13", ordernum: 112232, item: [{img: img, name: "한국시리즈 후드 105", money: 160000, refund: 157000, amt: 2}, {img: img, name: "한국시리즈 후드 105", money: 160000, refund: 157000, amt: 2}, , {img: img, name: "한국시리즈 후드 105", money: 160000, refund: 157000, amt: 2}], category: "단순변심", state: "환불요청"},  
-    
+    {id: 0, type: "환불", category: "단순변심",  state: "환불요청", date: "2023.11.13", ordernum: 112232, itemImg: img, itemName: "한국시리즈 후드 105", itemMoney: 160000, refund: 157000, amt: 2, username: "김디팡", userphone: "010-1111-1111", useradd: "경기도 성남시 수정구 성남대로 1342", usermessage: "문앞에 둘게요", refundmessage: "안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕"},
+    {id: 1, type: "환불", category: "단순변심",  state: "환불요청", date: "2023.11.13", ordernum: 112232, itemImg: img, itemName: "한국시리즈 후드 105", itemMoney: 160000, refund: 157000, amt: 2, username: "김디팡", userphone: "010-1111-1111", useradd: "경기도 성남시 수정구 성남대로 1342", usermessage: "문앞에 둘게요", refundmessage: "안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕"},
+    {id: 2, type: "환불", category: "단순변심",  state: "환불요청", date: "2023.11.13", ordernum: 112232, itemImg: img, itemName: "한국시리즈 후드 105", itemMoney: 160000, refund: 157000, amt: 2, username: "김디팡", userphone: "010-1111-1111", useradd: "경기도 성남시 수정구 성남대로 1342", usermessage: "문앞에 둘게요", refundmessage: "안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕"},
+    {id: 3, type: "환불", category: "단순변심",  state: "환불요청", date: "2023.11.13", ordernum: 112232, itemImg: img, itemName: "한국시리즈 후드 105", itemMoney: 160000, refund: 157000, amt: 2, username: "김디팡", userphone: "010-1111-1111", useradd: "경기도 성남시 수정구 성남대로 1342", usermessage: "문앞에 둘게요", refundmessage: "안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕안녕"},
+    {id: 4, type: "환불", category: "단순변심",  state: "환불요청", date: "2023.11.13", ordernum: 112232, itemImg: img, itemName: "한국시리즈 후드 105", itemMoney: 160000, refund: 157000, amt: 2, username: "김디팡", userphone: "010-1111-1111", useradd: "경기도 성남시 수정구 성남대로 1342", usermessage: "문앞에 둘게요", refundmessage: "안녕안녕안녕안녕"},
+    {id: 5, type: "환불", category: "단순변심",  state: "환불요청", date: "2023.11.13", ordernum: 112232, itemImg: img, itemName: "한국시리즈 후드 105", itemMoney: 160000, refund: 157000, amt: 2, username: "김디팡", userphone: "010-1111-1111", useradd: "경기도 성남시 수정구 성남대로 1342", usermessage: "문앞에 둘게요", refundmessage: "안녕안녕안녕안녕안녕안녕"},
 ]
 
 export default List;
---
+++ src/pages/user/myPage/Cancel/TableRow.js
@@ -0,0 +1,132 @@
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import { useState } from "react";
+
+
+const PaginationContainer = styled.div`
+
+width: 72rem;
+height: 5rem;
+display: flex;
+justify-content: center;
+align-items: center;
+
+
+`
+
+const Row = styled.div`
+  width: 72rem;
+  border-bottom: 1px black solid;
+  display: flex;
+  font-size: 14px;
+  
+`;
+
+const Col = styled.div`
+  width: ${(props) => props.width};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ItemImg = styled.img`
+width: 5rem;
+height: 5rem;
+
+`
+const ItemName = styled.div`
+
+width: 11rem;
+
+`
+const Column = styled.div`
+
+width: 10rem;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+
+`
+
+const ItemColBox = styled.div`
+
+display: flex;
+flex-direction: column;
+
+`
+
+const ItemCol = styled.div`
+
+height: 6rem;
+display: flex;
+${(props)=>setBorder(props.i)};
+border-left: 1px solid black;
+
+`
+
+const setBorder = (i) =>{
+
+  if(i!=0)
+  return {borderTop: "1px solid black"}
+  else
+  return {border: 0}
+  
+  }
+
+
+function TableRow(props) {
+
+  const navi = useNavigate();
+  const data = props.data;
+
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const itemsPerPage = 2;
+  const start = 
+
+  <>
+  {props.data.map((a) => {
+    return (
+      
+      <Row className="cm-SRegular16" onClick={()=>{navi(`/user/mypage/temp/refund/detail/${a.id}`)}}>
+        <Col width="9rem">
+          <Column>
+            <p>{a.date}</p>
+            <p>{a.ordernum}</p>
+          </Column>
+        </Col>
+        <Col width="8rem">{a.type}</Col>
+        <Col width="6rem">{a.category}</Col>
+        <Col width="9rem">{a.state}</Col>
+        <ItemColBox>
+          {a.item.map((b, i) => {
+
+            return (
+              <ItemCol i={i} key = {i}>
+                <Col width="22rem">
+                  <ItemImg src={b.img} />
+                  <ItemName>{b.name}</ItemName>
+                </Col>
+                <Col width="9rem">{b.money} / {b.amt}</Col>
+                <Col width="9rem">{b.refund}</Col>
+
+              </ItemCol>
+            )
+
+          })}
+        </ItemColBox>
+
+      </Row>
+    );
+  })};
+
+
+
+
+
+  </>
+}
+
+export default TableRow;
