c100e326a9af701b01c01673d04c18b1fb4fa09a
yougi8 <yougi8@naver.com>
2024-01-23 17:15:09 +0900

    [fix] 사용자 페이지 - 상품 상세보기 구현 완료

--- src/assets/datas/ProductAskData.js
+++ src/assets/datas/ProductAskData.js
@@ -5,9 +5,9 @@ export const ProductAskData = [
         date: '2024-01-17',
         title: '맹구 콧물이 돌아가는건가요?',
         userName: '김디팡',
-        askState: '답변 대기',
+        askState: '답변 완료',
         content: '제가 이거 사고싶은데 맹구 콧물이 진짜인가요? 진짜면 좀 사기 싫어서요 ㅠ',
-        answer: ''
+        answer: '고객님 안녕하세요. 콧물이 진짜일 수는 없습니다. 구매에 참고되시기 바랍니다.'
     },
     {
         askId: '2',
@@ -15,9 +15,14 @@ export const ProductAskData = [
         date: '2024-01-16',
         userName: '김지팡',
         title: '진짜 맹구도 같이 오나요?',
-        askState: '답변 완료',
+        askState: '답변 대기',
         content: '제가 맹구를 진짜 좋아하는데 혹시 진짜맹구도 같이 오는건가요? 그러면 이 가격이 말도 안되는 것 같은데',
         answer: ''
     },
+]
+
+export const ProductAskGuide = [
+    { context: '해당 상품에 대한 문의를 남기는 공간입니다' },
+    { context : '배송 관련 질문 사항은 마이페이지에서 1:1 문의에 등록해주세요' }
 ]
 
