9c6f6af9eb8e859a8fa081cb3c05fed87b47dd3c
jinii9 <jinii915@naver.com>
2024-01-23 15:25:32 +0900

    [version] Concurrent Mode -> render방식으로 수정

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import { GlobalStyle } from './styles/GlobalStyled';
 import '../src/styles/common.css';
 import '../src/styles/fontStyle.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle/>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
