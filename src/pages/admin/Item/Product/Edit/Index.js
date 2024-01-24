89ed85b348c71a85070a8a098b46ee0b87cbc1f7
yougi8 <yougi8@naver.com>
2024-01-24 14:18:16 +0900

    [feat] 관리자 페이지 - 상품 수정

--- src/pages/admin/Item/Product/Edit/Index.js
+++ src/pages/admin/Item/Product/Edit/Index.js
@@ -1,45 +1,63 @@
 import styled from "styled-components";
-import * as React from 'react';
-import { useState } from "react";
-import { useNavigate } from 'react-router-dom'
-import ProductDefaultEnroll from "./ProductDefaultEnroll";
-import ProductDetailEnroll from "./ProductDetailEnroll";
-
+import * as React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import ProductDefaultEdit from "./ProductDefaultEdit";
+import ProductDetailEnroll from "./ProductDetailEdit";
+import { useParams } from "react-router-dom";
+import ItemDetailData from "../../../../../assets/datas/ItemDetailData";
 // 상품 index 페이지
 const ProductEditPage = () => {
-    const navi = useNavigate();
-    const [productInfo, setProductInfo] = useState({
-        productName: '',
-        productPrice: '',
-        brandName: '',
-        productImage: '',
-        detailImage: '',
-        stock: '',
-    });
+  let itemId = useParams().id;
 
-    const handleAddBtn = () => {
-        console.log('상품 등록완료');
-        console.log(productInfo);
-    }
+  const navi = useNavigate();
+  const [itemInfo, setItemInfo] = useState(); // 상품 상세조회 할 id값 주소에서 가져오기
+  //   주소에서 가져온 id값과 일치하는 상품조회 데이터 가져오기
+  useEffect(() => {
+    const matchedItem = ItemDetailData.find((item) => item.id === itemId);
+    setItemInfo(matchedItem);
+  }, [itemId]);
+  //   const [productInfo, setProductInfo] = useState({
+  //     productName: "",
+  //     productPrice: "",
+  //     brandName: "",
+  //     productImage: "",
+  //     detailImage: "",
+  //     stock: "",
+  //   });
 
-    return (
-        <>
-            <Wrap>
-                <PageName className="cm-LBold30 col-Black"> 상품 관리 </PageName>
-                <PageSubName className='cm-MBold24 col-Navy'> 상품 수정</PageSubName>
-                {/* 이벤트 내용 입력하는 공간 */}
-                <InputSection>
-                <ProductDefaultEnroll productInfo={productInfo} setProductInfo={setProductInfo} />
-                    <ProductDetailEnroll productInfo={productInfo} setProductInfo={setProductInfo} />
-                    {/* 등록하기 버튼 */}
-                    <Button>
-                        <button className="Btn_S_Navy" onClick={() => handleAddBtn()}>추가하기</button>
-                    </Button>
-                </InputSection>
+  const handleAddBtn = () => {
+    console.log("상품 수정완료");
+    console.log(itemInfo);
+  };
 
-            </Wrap>
-        </>
-    );
+  return (
+    <>
+      {itemInfo && (
+        <Wrap>
+          <PageName className="cm-LBold30 col-Black"> 상품 관리 </PageName>
+          <PageSubName className="cm-MBold24 col-Navy"> 상품 수정</PageSubName>
+          {/* 상품 내용 입력하는 공간 */}
+          <InputSection>
+            <ProductDefaultEdit
+              productInfo={itemInfo}
+              setProductInfo={setItemInfo}
+            />
+            <ProductDetailEnroll
+              productInfo={itemInfo}
+              setProductInfo={setItemInfo}
+            />
+            {/* 수정하기 버튼 */}
+            <Button>
+              <button className="Btn_S_Navy" onClick={() => handleAddBtn()}>
+                수정하기
+              </button>
+            </Button>
+          </InputSection>
+        </Wrap>
+      )}
+    </>
+  );
 };
 
 export default ProductEditPage;
@@ -56,22 +74,22 @@ const PageName = styled.div`
   align-items: center;
 `;
 const PageSubName = styled.div`
-    display: flex;
-    width: 88.9375rem;
-    box-sizing: border-box; // padding까지 합쳐서 width 설정하기
-    padding: 2.125rem 7.5rem 0.9375rem 8.5rem;
-    align-items: center;
-    font-size: 1.5rem;
-`
+  display: flex;
+  width: 88.9375rem;
+  box-sizing: border-box; // padding까지 합쳐서 width 설정하기
+  padding: 2.125rem 7.5rem 0.9375rem 8.5rem;
+  align-items: center;
+  font-size: 1.5rem;
+`;
 const InputSection = styled.div`
-    display: flex;
-    width: 88.9375rem;
-    box-sizing: border-box; // padding까지 합쳐서 width 설정하기
-    padding: 0rem 7.5rem 6rem 7.5rem;
-    flex-direction: column;
-    align-items: center;
-    gap: -0.0625rem;
-`
+  display: flex;
+  width: 88.9375rem;
+  box-sizing: border-box; // padding까지 합쳐서 width 설정하기
+  padding: 0rem 7.5rem 6rem 7.5rem;
+  flex-direction: column;
+  align-items: center;
+  gap: -0.0625rem;
+`;
 const Button = styled.div`
-    display: flex;
+  display: flex;
 `;
---
+++ src/pages/admin/Item/Product/Edit/ProductDetailEdit.js
@@ -0,0 +1,201 @@
+import styled from "styled-components";
+import * as React from "react";
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Fab from "@mui/material/Fab";
+import PhotoIcon from "@mui/icons-material/Photo";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+const ProductDetailEnroll = ({ productInfo, setProductInfo }) => {
+  const [itemImg, setItemImg] = useState();
+  const [infoImg, setInfoImg] = useState();
+
+  const [isHovered, setIsHovered] = useState(false);
+  // 상품정보 이미지삭제 감지
+  const handleItemImgDelete = (e) => {
+    setItemImg(null);
+  };
+  // 상세정보 이미지삭제 감지
+  const handleInfoImgDelete = (e) => {
+    setInfoImg(null);
+  };
+  return (
+    <Wrap>
+      <div className="cm-SBold18 col-Navy">상품 상세 정보</div>
+      {/* 상품 정보 입력 칸 */}
+      <Table>
+        {/* 상품 이미지  */}
+        <Row>
+          <p className="cm-SBold16 col-Black">상품 이미지</p>
+          <ContentBox>
+            {itemImg ? (
+              <div
+                style={{ position: "relative" }}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+              >
+                <img
+                  src={infoImg}
+                  alt="상품 이미지"
+                  style={{ width: "100%", height: "auto" }}
+                />
+                {isHovered && (
+                  <div
+                    style={{
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      width: "100%",
+                      height: "100%",
+                      backgroundColor: "rgba(0, 0, 0, 0.5)",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      transition: "opacity 0.3s",
+                      cursor: "pointer",
+                    }}
+                  >
+                    {/* 사진에 마우스 hover시, 삭제아이콘 등장 */}
+                    <button
+                      onClick={handleItemImgDelete}
+                      style={{ backgroundColor: "transparent", border: "none" }}
+                    >
+                      <DeleteIcon style={{ color: "var(--light-grey)" }} />{" "}
+                    </button>
+                  </div>
+                )}
+              </div>
+            ) : (
+              // 사진 선택하는 로직 넣기
+              <Fab
+                color="inherit"
+                aria-label="add"
+                sx={{ maxWidth: "40px", maxHeight: "40px" }}
+              >
+                <PhotoIcon color="var(--light-grey)" />
+              </Fab>
+            )}
+          </ContentBox>
+        </Row>
+        {/* 상세 이미지  */}
+        <Row>
+          <p className="cm-SBold16 col-Black">상세 이미지</p>
+          <ContentBox>
+            {infoImg ? (
+              <div
+                style={{ position: "relative" }}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+              >
+                <img
+                  src={infoImg}
+                  alt="상세정보 이미지"
+                  style={{ width: "100%", height: "auto" }}
+                />
+                {isHovered && (
+                  <div
+                    style={{
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      width: "100%",
+                      height: "100%",
+                      backgroundColor: "rgba(0, 0, 0, 0.5)",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      transition: "opacity 0.3s",
+                      cursor: "pointer",
+                    }}
+                  >
+                    {/* 사진에 마우스 hover시, 삭제아이콘 등장 */}
+                    <button
+                      onClick={handleInfoImgDelete}
+                      style={{ backgroundColor: "transparent", border: "none" }}
+                    >
+                      <DeleteIcon style={{ color: "var(--light-grey)" }} />{" "}
+                    </button>
+                  </div>
+                )}
+              </div>
+            ) : (
+              // 사진 선택하는 로직 넣기
+              <Fab
+                color="inherit"
+                aria-label="add"
+                sx={{ maxWidth: "40px", maxHeight: "40px" }}
+              >
+                <PhotoIcon color="var(--light-grey)" />
+              </Fab>
+            )}
+          </ContentBox>
+        </Row>
+      </Table>
+    </Wrap>
+  );
+};
+
+export default ProductDetailEnroll;
+const Wrap = styled.div`
+  display: flex;
+  width: 88.9375rem;
+  box-sizing: border-box; // padding까지 합쳐서 width 설정하기
+  padding: 0rem 10rem 6.25rem 10rem;
+  flex-direction: column;
+`;
+const Section = styled.div`
+  display: flex;
+  box-sizing: border-box; // padding까지 합쳐서 width 설정하기
+  padding-top: 1rem;
+  flex-direction: column;
+  align-items: center;
+  gap: -0.0625rem;
+`;
+const ContentBox = ({ children }) => (
+  <div style={{ width: "100%", margin: "1rem" }}>
+    <Box
+      sx={{
+        "& > :not(style)": { m: 0, width: "100%" },
+        display: "flex",
+        flexDirection: "column",
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      {children}
+    </Box>
+  </div>
+);
+const Table = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 69rem;
+  box-sizing: border-box; // padding까지 합쳐서 width 설정하기
+  // padding: 0rem 7.5rem 1rem 7.5rem;
+  padding-top: 1rem;
+`;
+const Row = styled.div`
+  border-top: 1px solid var(--black);
+  &:last-child {
+    border-bottom: 1px solid var(--black);
+  }
+  display: flex;
+  p {
+    background: var(--light-grey, #f4f4f4);
+    /* padding: 2rem; */
+    width: 13rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+  }
+`;
+const Content = styled.div`
+  width: 100%;
+  margin: 1rem;
+`;
+const Submit = styled.div`
+  padding-top: 3rem;
+  display: flex;
+  justify-content: flex-end;
+`;
