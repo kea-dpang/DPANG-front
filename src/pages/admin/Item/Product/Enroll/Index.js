6aa6fde930332b4a1a001c4abbe4a016cfb083ca
yougi8 <yougi8@naver.com>
2024-01-24 11:49:02 +0900

    [feat] 관리자 - 상품 등록 페이지

--- src/pages/admin/Item/Product/Enroll/Index.js
+++ src/pages/admin/Item/Product/Enroll/Index.js
@@ -8,17 +8,35 @@ import ProductDetailEnroll from "./ProductDetailEnroll";
 // 상품 이벤트 등록 index 페이지
 const ProductEnrollPage = () => {
     const navi = useNavigate();
+    const [productInfo, setProductInfo] = useState({
+        productName: '',
+        productPrice: '',
+        brandName: '',
+        productImage: '',
+        detailImage: '',
+        stock: '',
+    });
+
+    const handleAddBtn = () => {
+        console.log('상품 등록완료');
+        console.log(productInfo);
+    }
 
     return (
-        <>  
+        <>
             <Wrap>
                 <PageName className="cm-LBold30 col-Black"> 상품 관리 </PageName>
                 <PageSubName className='cm-MBold24 col-Navy'> 상품 등록</PageSubName>
                 {/* 이벤트 내용 입력하는 공간 */}
                 <InputSection>
-                    <ProductDefaultEnroll />
-                    <ProductDetailEnroll/>
+                <ProductDefaultEnroll productInfo={productInfo} setProductInfo={setProductInfo} />
+                    <ProductDetailEnroll productInfo={productInfo} setProductInfo={setProductInfo} />
+                    {/* 등록하기 버튼 */}
+                    <Button>
+                        <button className="Btn_S_Navy" onClick={() => handleAddBtn()}>추가하기</button>
+                    </Button>
                 </InputSection>
+
             </Wrap>
         </>
     );
@@ -49,8 +67,11 @@ const InputSection = styled.div`
     display: flex;
     width: 88.9375rem;
     box-sizing: border-box; // padding까지 합쳐서 width 설정하기
-    padding: 0rem 7.5rem 6.25rem 7.5rem;
+    padding: 0rem 7.5rem 6rem 7.5rem;
     flex-direction: column;
     align-items: center;
     gap: -0.0625rem;
 `
+const Button = styled.div`
+    display: flex;
+`;
