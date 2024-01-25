38b1b3129eb53264ca3f0174c01b7989f36bb245
hogishim <harrythegreat@gachon.ac.kr>
2024-01-25 18:45:43 +0900

    [fix]사용자 마이페이지 - 취소 상세 보기 페이지 펼침 박스 구현

--- src/pages/user/myPage/Cancel/Detail/Refund/Table.js
+++ src/pages/user/myPage/Cancel/Detail/Refund/Table.js
@@ -1,4 +1,7 @@
+import DetailRefund from "pages/user/myPage/Cancel/Detail/Refund/DetailRefund";
 import styled from "styled-components";
+import DetailHeader from "components/utils/DetailTable/DetailTableTitle";
+import { useState } from "react";
 
 const Wrap = styled.div`
   width: 71rem;
@@ -6,77 +9,26 @@ const Wrap = styled.div`
   flex-direction: column;
 `;
 
-const Container = styled.div`
-  width: 72rem;
-`;
-const Border = styled.div`
-  height: 1px;
-  width: 72rem;
-  background-color: black;
-`;
-
-const TableBox = styled.div`
-  height: 20rem;
-  width: 72rem;
-`;
-const Row = styled.div`
-  height: ${(props)=>props.height};
-  width: 72rem;
-  display: flex;
-`;
-const ColHeader = styled.div`
-  width: 10rem;
-  height: 100%;
-  background-color: var(--light-grey);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const Col = styled.div`
-  height: 100%;
-  width: ${(props) => props.width};
-  display: flex;
-  align-items: center;
-
-`;
-const DetailHeader = styled.div`
-  width: 72rem;
+const Blank = styled.div`
   height: 5rem;
-  background-color: white;
-  display: flex;
-  align-items: end;
 `;
 
 function Table(props) {
+  const [clickState, setClickState] = useState(false);
+
+  const handleclick = () => {
+    setClickState(!clickState);
+  };
+
   return (
     <Wrap>
-
-
-      <DetailHeader className="cm-SRegular18">결제 상세 내역</DetailHeader>
-      <Container>
-        <Border />
-        <TableBox className="cm-SRegular16">
-          <Row height="6rem">
-            <ColHeader>상품 합계 금액</ColHeader>
-            <Col width="26rem">{props.data.itemMoney}</Col>
-            <ColHeader>환불 예정 금액</ColHeader>
-            <Col width="26rem">{props.data.refund}</Col>
-          </Row>
-
-          <Border />
-
-          <Row height="6rem">
-            <ColHeader>환불 상태</ColHeader>
-            <Col width="62rem">{props.data.state}</Col>
-          </Row>
-          <Border />
-        </TableBox>
-      </Container>
-
-
-      
-      <DetailHeader />
+      <Blank />
+      <DetailHeader
+        width="71rem"
+        text="결제 상세 내역"
+        handleclick={handleclick}
+      />
+      {clickState && <DetailRefund data={props.data} />}
     </Wrap>
   );
 }
