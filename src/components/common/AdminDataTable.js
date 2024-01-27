6d4bf80934346e26a69ecd9cdf50db88507c10e3
hogishim <harrythegreat@gachon.ac.kr>
2024-01-27 20:49:11 +0900

    [fix]관리자 페이지 - 취소 조회 페이지 공용 컴포넌트 이용하여 재구성

--- src/components/common/AdminDataTable.js
+++ src/components/common/AdminDataTable.js
@@ -2,8 +2,9 @@ import React from "react";
 import MUIDataTable from "mui-datatables";
 import { askManageData } from "../../assets/data/admin/AdminAskData";
 
-const DataTable = ({ data, columns, onRowClick }) => {
-  console.log(data);
+const DataTable = ({ data, columns, setRowProps, onRowClick }) => {
+
+  console.log("DataTable props:", { data, columns, setRowProps, onRowClick });
 
   const options = {
     selectableRows: "multiple",
@@ -13,26 +14,12 @@ const DataTable = ({ data, columns, onRowClick }) => {
       return false; // prevent the default delete action
     },
     onRowClick: (rowData, rowMeta) => {
-      // state 값이 '답변완료'일 경우 클릭 이벤트를 무시합니다.
-      // if (rowData[3] === '답변완료') {
-      //     console.log('답변완료 상태의 행은 클릭할 수 없습니다.');
-      //     return;
-      // }
-      // console.log(rowData)
       onRowClick(rowData); // 클릭된 행의 데이터를 부모 컴포넌트로 전달
     },
-    // setRowProps: (row, dataIndex, rowIndex) => {
-    //   if (askManageData[dataIndex].state === '답변완료') {
-    //       // '답변완료' 상태의 행에 대해 투명도를 조절하여 비활성화처럼 보이게 합니다.
-    //       return {
-    //           style: { backgroundColor: 'var(--dark-grey)', opacity: 0.5, pointerEvents: 'none' },
-    //       };
-    //   }
-    // },
+    setRowProps: setRowProps, // 외부에서 받아온 setRowProps 함수 사용
   };
-
   return (
-    <div style={{ height: 400, width: "100%" }}>
+    <div style={{ width: "100%" }}>
       <MUIDataTable data={data} columns={columns} options={options} />
     </div>
   );
