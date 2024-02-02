4374bfe6f26e4052b31dd2be02dec3fd2a748e29
hogishim <harrythegreat@gachon.ac.kr>
2024-02-02 13:47:10 +0900

    [feat]주문 취소, 주문 환불 API 미리 작성

---
+++ src/api/refund.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+
+const url = "api/refund";
+
+//특정 제품에 대한 반품 요청을 날리는 API
+const POST_refund_order = async (inputValue) => {
+  //서버에 전달해줄 값은 어떤 order ID에 대한 것인지와 취소 사유, 취소 관련 남길 말, 그리고 회수시 배송 요청사항이다
+  const res = await axios({
+    method: "post",
+    url: `${url}/${inputValue.orderId}`,
+    data: {
+      refundCategory: inputValue.refundCategory,
+      refundMessage: inputValue.refudMessage || undefined,
+      refundShipmentMessage: inputValue.refundShipmentMessage,
+    },
+  });
+  return res.data;
+};
+
+//특정 환불에 대한 목록을 조회하기 위한 API
+const GET_refund_detail = async (refundId) => {
+  //서버에게 전달해줄 값은 어떤 refund에 대한 것인지이다
+  const res = await axios({
+    method: "get",
+    url: `${url}/${refundId}`,
+  });
+  return res.data;
+};
+
+//환불에 대한 목록을 조회하기 위한 API
+const GET_refund_list = async (inputValue) => {
+  //서버에게 전달해줄 값은 어떤 user인지, 조회 시작 기간, 조회 종료 기간, 환불 사유, 페이지 네이션 관련 내용들이다
+  const res = await axios({
+    method: "get",
+    url: `${url}/list`,
+    params: {
+      userId: inputValue.userId || undefined,
+      startDate: inputValue.startDate || undefined,
+      endDate: inputValue.endDate || undefined,
+      reason: inputValue.reason || undefined,
+      page: inputValue.page || undefined,
+      size: inputValue.size || 10,
+      sort: inputValue.sort || undefined,
+    },
+  });
+  return res.data;
+};
+
+//환불에 대한 진행상태를 다음 단계로 업데이트 시키게 하기 위한 서버로의 API 요청
+const PUT_update_status = async (orderId) => {
+  const res = await axios({
+    method: "put",
+    url: `${url}/${orderId}`,
+  });
+  return res.data;
+};
