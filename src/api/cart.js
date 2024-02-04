243c0253d48e9f5df8090c34b9ae28907c1134a5
yougi8 <yougi8@naver.com>
2024-02-04 19:58:55 +0900

    [feat] 장바구니 API 생성

---
+++ src/api/cart.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+const url = `/api/carts`;
+
+// TODO: 로컬스토리지에서 사용자Id 가져오기
+//장바구니 담기
+export const POST_Cart = async (userId, itemId) => {
+  console.log("장바구니 담기입니당: ", itemId);
+  const res = await axios({
+    method: "post",
+    url: `${url}/${userId}/${itemId}`,
+  });
+  return res.data;
+};
+
+// 장바구니 목록 조회
+export const GET_CartList = async (userId) => {
+  console.log("장바구니 목록 조회입니당");
+  const response = await axios({
+    method: "get",
+    url: `${url}/${userId}`,
+  });
+  return res.data;
+};
+// 장바구니 목록 조회
+export const DELETE_Cart = async (userId) => {
+  console.log("장바구니 상품 삭제합니당");
+  const response = await axios({
+    method: "delete",
+    url: `${url}/${userId}/${itemId}`,
+  });
+  return res.data;
+};
