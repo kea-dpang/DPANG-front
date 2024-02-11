96d0910edb6b0e90de753eb2afde8d44356630ba
jinii9 <jinii915@naver.com>
2024-02-12 00:35:08 +0900

    [refactor] react-query 환경 설정

--- src/index.js
+++ src/index.js
@@ -5,13 +5,21 @@ import { GlobalStyle } from "./styles/GlobalStyled";
 import "../src/styles/common.css";
 import "../src/styles/fontStyle.scss";
 import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
-  <RecoilRoot>
-    <React.StrictMode>
-      <GlobalStyle />
-      <App />
-    </React.StrictMode>
-  </RecoilRoot>,
+  <QueryClientProvider client={queryClient}>
+    <RecoilRoot>
+      <React.StrictMode>
+        <GlobalStyle />
+        <App />
+      </React.StrictMode>
+    </RecoilRoot>
+
+    <ReactQueryDevtools />
+  </QueryClientProvider>,
   document.getElementById("root")
 );
